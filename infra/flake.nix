a42592555a3af236bccdaac6a18e23db00fe4e27
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-06-28 16:24:39 -0700

    `nix run nixpkgs#nixpkgs-fmt --  flake.nix`

--- infra/flake.nix
+++ infra/flake.nix
@@ -11,21 +11,24 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat}:
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = let
-        aws-bootstrap-pkgs = (import ./utils/aws-bootstrap/default-system.nix) system;
-        in [
-          pkgs.terraform
-          pkgs.awscli2
-          aws-bootstrap-pkgs.default
-        ];
-      };
-  });
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            let
+              aws-bootstrap-pkgs = (import ./utils/aws-bootstrap/default-system.nix) system;
+            in
+            [
+              pkgs.terraform
+              pkgs.awscli2
+              aws-bootstrap-pkgs.default
+            ];
+        };
+      });
 }
