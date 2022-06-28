6a31b6fba00d94da117af893b49524ab42c74f29
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-06-28 00:27:01 -0700

    Try no `callPackage`

--- infra/flake.nix
+++ infra/flake.nix
@@ -9,27 +9,24 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-
-    # aws-bootstrap = {
-    #   url = "./utils/aws-bootstrap/";
-    # };
-    # aws-bootstrap.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, flake-compat}:
     flake-utils.lib.eachDefaultSystem (system:
     let
+      overlay = self: super: {
+        aws-bootstrap = ./utils/aws-bootstrap/default.nix;
+      };
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [ overlay ];
       };
-      aws-bootstrap = pkgs.callPackage ./utils/aws-bootstrap/default.nix {};
     in {
-
       devShell = pkgs.mkShell {
         buildInputs = [
           pkgs.terraform
           pkgs.awscli2
-          aws-bootstrap
+          pkgs.aws-bootstrap
         ];
       };
   });
