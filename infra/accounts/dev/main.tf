931dbb87df2826ab6a480bf5096d91527cc98f4f
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-07-01 20:12:03 -0700

    Prevent all ami buckets from ever being public

--- infra/accounts/dev/main.tf
+++ infra/accounts/dev/main.tf
@@ -20,3 +20,12 @@ resource "aws_s3_bucket" "images" {
     Name = "AMI Images"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
